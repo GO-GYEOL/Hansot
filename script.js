5157f7b7142fe471b8892f537e20e944dadf3352
GO-GYEOL <rhruf3297@gmail.com>
2022-03-12 19:18:36 +0900

    메뉴추가

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(".navbar>.menu").hover(function () {
+    $(".navbar > .sub-menu").toggleClass("active");
+  });
+  $(".scroll-navbar>.menu").hover(function () {
+    $(".scroll-navbar > .sub-menu").toggleClass("active");
+  });
+
+
+  $(window).scroll(function () {
+    var scroll_top = $(this).scrollTop();
+    if (scroll_top >= 3) {
+      $(".navbar").removeClass("active");
+      $(".scroll-navbar-wrap").addClass("active");
+    }
+    if ((scroll_top < 2)) {
+      $(".navbar").addClass("active");
+      $(".scroll-navbar-wrap").removeClass("active");
+    }
+  });
+});
