418d5f47c22d96b4cbcc8734e1158da5d7d2dd80
GO-GYEOL <rhruf3297@gmail.com>
2022-03-13 11:11:08 +0900

    z-index

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ $(document).ready(function () {
     $(".scroll-navbar > .sub-menu").toggleClass("active");
   });
 
-
   $(window).scroll(function () {
     var scroll_top = $(this).scrollTop();
     if (scroll_top >= 3) {
       $(".navbar").removeClass("active");
       $(".scroll-navbar-wrap").addClass("active");
     }
-    if ((scroll_top < 2)) {
+    if (scroll_top < 2) {
       $(".navbar").addClass("active");
       $(".scroll-navbar-wrap").removeClass("active");
     }
+  });
+});
+
+$(document).ready(function () {
+  $(".slider-box").slick({
+    // autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    arrows: false,
   });
 });
